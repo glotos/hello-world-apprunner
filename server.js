fcd40f48a8d27f494983340df56907bf163bc9ca
glotos <143279120+glotos@users.noreply.github.com>
2025-02-12 02:23:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello, World! Welcome to AWS App Runner");
+    });
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+    
